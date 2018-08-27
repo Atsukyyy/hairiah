c6bb3ab00eef9e7c9e25c0928d389b307eb0140f
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-08-27 12:47:54 +0900

    image

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -12,8 +12,14 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.development?
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    elsif Rails.env.production?
+      "/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
+
+
 
   # cacheでアップロードする画像の設定
   def cache_dir
