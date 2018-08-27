15d40d25fc445e3daf263e79ae59b6922f2bfcfa
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-08-27 13:18:21 +0900

    cw

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,10 +11,9 @@ if Rails.env.production?
     config.fog_directory     =  ENV['S3_BUCKET']
   end
   case Rails.env
-    when 'development'
-        config.asset_host = 'https://s3.amazonaws.com/ap-northeast-1'
-    when 'production'
-        config.asset_host = 'https://s3.amazonaws.com/ap-northeast-1'
-    end
+  when 'development'
+      config.asset_host = 'https://s3.amazonaws.com/ap-northeast-1'
+  when 'production'
+      config.asset_host = 'https://s3.amazonaws.com/ap-northeast-1'
   end
 end
