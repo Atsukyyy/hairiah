381acaf539506b527366045eef9debb665bab98f
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-08-29 00:10:31 +0900

    環境変数

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 # OmniAuth.logger.progname = "omniauth"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1006946089485811', 'f663e938d625a243f67d7412535e4c14',
+  provider :facebook, ENV['FB_KEY'], ENV['FB_SEC'],
     scope: 'email, public_profile',
     info_fields: 'email, first_name, last_name'
   # provider :line, '1511152097', '9d0d7f2b16a97f75f2f320a35bb37b2b'
