241750ba6e2b51d5ef21ea52d3d503c85678bb79
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-08-29 09:41:56 +0900

    header

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
+  var state = false;
+  var scrollpos;
+
   $(".hamburger-open-btn").on("click", ()=> {
-      $(".hamburger-menu").slideToggle(500);
+    scrollpos = $(window).scrollTop();
+      $('body').addClass('fixed').css({'top': -scrollpos});
+      $(".hamburger-menu").slideToggle(150);
       $(".hamburger-close-btn").css("display", "inline-block");
       $(".hamburger-open-btn").css("display", "none")
+      $(".header-fix").css("position", "fixed");
+      state = true;
   });
 
   $(".hamburger-close-btn").on("click", ()=> {
-    $(".hamburger-menu").slideToggle(500);
+    $('body').removeClass('fixed').css({'top': 0});
+      window.scrollTo( 0 , scrollpos );
+    $(".hamburger-menu").slideToggle(150);
     $(".hamburger-open-btn").css("display", "inline-block");
     $(".hamburger-close-btn").css("display", "none")
+    $(".header-fix").css("position", "inherit");
+    state = false;
   });
 });
