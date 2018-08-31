07a7c936516d1c37db195f23aa259846ef7d4a6e
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-09-01 00:50:33 +0900

    redos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://peaceful-spire-45775.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://peaceful-spire-45775.herokuapp.com' ]
-  if ENV["REDISCLOUD_URL"]
-    uri = URI.parse(ENV["REDISCLOUD_URL"])
-    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  end
+  config.action_cable.url = "wss://peaceful-spire-45775.herokuapp.com/cable"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
