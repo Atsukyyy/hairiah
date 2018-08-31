9bcd29a05578c9b0eba8d811c739e7676848434b
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-09-01 02:15:44 +0900

    hide resque & add addressable

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 # ENV['REDIS_URL'] = ENV["REDISCLOUD_URL"] if ENV["REDISCLOUD_URL"]
+require "addressable/uri"
 
 if ENV["REDISCLOUD_URL"]
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  uri = Addressable::URI.parse(ENV["REDISCLOUD_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
