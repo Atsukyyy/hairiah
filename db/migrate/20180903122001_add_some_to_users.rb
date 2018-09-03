1aa75f251d148fd41782c945e5f32d9585df8e70
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-09-03 23:08:04 +0900

    fi

--- db/migrate/20180903122001_add_some_to_users.rb
+++ db/migrate/20180903122001_add_some_to_users.rb
@@ -3,7 +3,5 @@ class AddSomeToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :fb_sign_up, :boolean
     add_column :users, :g_sign_up, :boolean
     add_column :users, :use, :boolean
-    add_column :users, :qualification, :date
-    add_column :users, :experience, :integer
   end
 end
