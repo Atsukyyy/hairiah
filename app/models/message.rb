86ce696d0168d37b1c47d9a5d7311cdd924f9042
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-09-27 21:00:07 +0900

    message_box_design

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,7 @@ class Message < ApplicationRecord
   validates :from_id, presence: true
   validates :to_id, presence: true
   validates :room_id, presence: true
-  validates :content, presence: true, length: {maximum: 50}
+  validates :content, presence: true
 
   # Callbacks
   after_create_commit { MessageBroadcastJob.perform_later self }
