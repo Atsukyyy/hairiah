8e4c2a07a46bbffdf26a8a3a4d81225e68cb731a
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-10-07 23:45:45 +0900

    whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
-set :output, 'log/crontab.log'
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+set :output, 'log/cron.log'
+
 every 1.minute do
   command "echo '============================================================='"
   command "echo 'テスト'"
